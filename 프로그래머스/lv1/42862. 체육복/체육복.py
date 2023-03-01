b6da09849967b6a14f5c65d0bd96a44ab224b2d7
Jisup.Kim <102848253+jisup-kim@users.noreply.github.com>
2023-03-01 16:18:22 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,39 +1,36 @@
 def solution(n, lost, reserve):
-    having_list = [1] * (n+1)
-    print(having_list)
+    having_list = [1] * (n+1) # 체육복 보유 개수 리스트 초기화
     
-    for x_lost in lost:
+    for x_lost in lost: # 잃어버린 사람 체크
         having_list[x_lost] -= 1
-    for x_reserve in reserve:
+    for x_reserve in reserve: # 여분있는 사람 체크
         having_list[x_reserve] += 1
-    print(having_list)
     
-    for check_list in range(1,n+1):
-        if having_list[check_list] >= 1:
+    for check_list in range(1,n+1): # 체육복 리스트 불러오기
+        if having_list[check_list] >= 1: # 빌릴 필요 없으면 pass
             pass
-        else: # having_list[check_list] == 0
-            if check_list == 1:
+        else: # having_list[check_list] == 0 [빌릴 사람]
+            if check_list == 1: # 체격이 제일 작은 사람 [최소 케이스]
                 if having_list[check_list+1] >= 2:
                     having_list[check_list] += 1
                     having_list[check_list+1] -= 1
-            elif check_list == n:
+            elif check_list == n: # 체격이 제일 큰 사람 [최대 케이스]
                 if having_list[check_list-1] >= 2:
                     having_list[check_list] += 1
                     having_list[check_list-1] -= 1
-            else:
-                if having_list[check_list-1] >= 2:
+            else: # 양 옆에 사람이 있는 경우
+                if having_list[check_list-1] >= 2: # 작은 사람한테 먼저 빌리기
                     having_list[check_list] += 1
                     having_list[check_list-1] -= 1
-                elif having_list[check_list+1] >= 2:
+                elif having_list[check_list+1] >= 2: # 큰 사람한테 빌리기
                     having_list[check_list] += 1
                     having_list[check_list+1] -= 1
                 else:
                     print("빌려 줄 체육복이 양쪽 다 없습니다.")
-    print(having_list)
     
     check_student = 0
     for i in range(1,n+1):
         if having_list[i] >= 1:
             check_student += 1
-    answer = check_student
-    return answer
+
+    return check_student
